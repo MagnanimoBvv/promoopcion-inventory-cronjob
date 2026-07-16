ec8dfd1944fb562d4b828dc6f599e541e92e044a
Alexis Contreras <contrerasalexis380@gmail.com>
2026-07-16 11:14:01 -0600

    refactor for queued execution, missing variants and fetch shopify products in bulk

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const axios = require('axios');
-const { getLocationId, getProductByHandle, updateInventory } = require('./shopifyFunctions');
+const { getLocationId, paginateProductsByVendor, updateInventory } = require('./shopifyFunctions');
 
 async function getPromoOpcionProducts() {
     const response = await axios.post(
@@ -34,52 +34,68 @@ async function getPromoOpcionInventory() {
     return response.data;
 }
 
-async function updateProducts() {
-    const responseProducts = await getPromoOpcionProducts();
-    if (!responseProducts.success) return;
+function getStores() {
+    const storeNames = process.env.STORES.split(',');
 
-    const responseInventory = await getPromoOpcionInventory();
-    if (!responseInventory.success) return;
+    return storeNames.map(name => ({
+        name,
+        graphqlUrl: process.env[`GRAPHQL_URL_${name}`],
+        shopifyToken: process.env[`SHOPIFY_TOKEN_${name}`],
+    }));
+}
 
-    const locationId = await getLocationId();
-    const products = responseProducts.response || responseProducts.respusta;
+async function updateProducts(store, products, inventory) {
+    const locationId = await getLocationId(store);
+    const shopifyProducts = await paginateProductsByVendor(store, 'PromoOpcion');
     for (const product of products) {
         try {
             // if (product.skuPadre !== 'PET 008') continue; // If para pruebas con un producto específico
             const handle = `po-${product.skuPadre}`.trim().toLowerCase().replace(/-+$/g, '').replace(/[\s/]+/g, '-'); // Reemplaza espacios y diagonales y quita guiones al final
-            const shopifyProduct = await getProductByHandle(handle);
-            if (!shopifyProduct) {
-                continue;
-            }
+            const shopifyProduct = shopifyProducts.find(p => p.handle === handle);
+            if (!shopifyProduct) continue;
 
-            const activeVariants = product.hijos;
             const shopifyVariants = shopifyProduct.variants.nodes;
-            for (const activeVariant of activeVariants) {
-                const variant = shopifyVariants.find(v => v.sku === activeVariant.skuHijo);
-                const vendorInventory = responseInventory.Stocks.filter(item => item.Material === activeVariant.skuHijo);
-                const variantInventory = vendorInventory.reduce((acum, item) => acum + item.Stock, 0); // Suma el inventario de todas las ubicaciones
-                console.log(`Variante encontrada: ${shopifyProduct.title} ${variant.title}, Inventario: Prev ${variant.inventoryQuantity} Now ${variantInventory}`);
+            const activeVariants = product.hijos;
+            const activeVariantBySKU = new Map(activeVariants.map(v => [v.skuHijo, v]));
 
-                if (variant.inventoryQuantity !== variantInventory) {
-                    const variantToUpdate = {
-                        quantities: {
-                            changeFromQuantity: null,
-                            inventoryItemId: variant.inventoryItem.id,
-                            locationId,
-                            quantity: variantInventory,
-                        },
-                        name: "available",
-                        reason: "correction",
-                    };
-                    const response = await updateInventory(variantToUpdate);
-                    console.log('Inventario actualizado:', response.changes);
-                }
+            for (const variant of shopifyVariants) {
+                const activeVariant = activeVariantBySKU.get(variant.sku);
+                const targetInventory = activeVariant && activeVariant.estatus === '1' ? inventory.filter(i => i.Material === activeVariant.skuHijo).reduce((acum, i) => acum + i.Stock, 0) : 0;
+                const label = activeVariant && activeVariant.estatus === '1' ? 'Variante existente' : 'Variante faltante';
+                console.log(`[${store.name}] ${label}: ${shopifyProduct.title} ${variant.title}, Prev ${variant.inventoryQuantity} Now ${targetInventory}`);
+
+                if (variant.inventoryQuantity === targetInventory) continue;
+
+                const variantToUpdate = {
+                    quantities: {
+                        changeFromQuantity: null,
+                        inventoryItemId: variant.inventoryItem.id,
+                        locationId,
+                        quantity: targetInventory,
+                    },
+                    name: "available",
+                    reason: "correction",
+                };
+                const response = await updateInventory(store, variantToUpdate);
+                console.log(`[${store.name}] Inventario actualizado:`, response.changes);
             }
             // break;
         } catch (error) {
-            console.error(`Error actualizando el producto ${product.nombrePadre} ${product.skuPadre}:`, error);
+            console.error(`[${store.name}] Error actualizando ${product.nombrePadre} ${product.skuPadre}:`, error);;
         }
     }
 }
 
-updateProducts();
+async function main() {
+    const products = await getPromoOpcionProducts();
+    if (!products.success) return;
+    const inventory = await getPromoOpcionInventory();
+    if (!inventory.success) return;
+
+    const stores = getStores();
+    for (const store of stores) {
+        await updateProducts(store, products.response, inventory.Stocks);
+    }
+}
+
+main();
--- shopifyFunctions.js
+++ shopifyFunctions.js
@@ -1,96 +1,96 @@
 const axios = require('axios');
 
-async function getLocationId() {
+async function shopifyGraphQL(store, query, variables) {
     const response = await axios.post(
-        process.env.GRAPHQL_URL,
-        JSON.stringify({
-            query: `
-                query {
-                    locations(first: 10) {
+        store.graphqlUrl,
+        JSON.stringify({ query, variables }),
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Shopify-Access-Token': store.shopifyToken,
+            }
+        }
+    );
+
+    return response.data.data;
+}
+
+async function getLocationId(store) {
+    const data = await shopifyGraphQL(store, `
+        query {
+            locations(first: 10) {
+                nodes {
+                    id
+                    name
+                }
+            }
+        }
+    `);
+
+    return data.locations.nodes[0].id;
+}
+
+async function getProductsByVendor(store, cursor, vendor) {
+    const data = await shopifyGraphQL(store, `
+        query {
+            products(first: 100, ${cursor ? `after: "${cursor}",` : ''} query: "vendor:${vendor} status:active") {
+                pageInfo { hasNextPage endCursor }
+                nodes {
+                    handle
+                    title
+                    variants(first: 150) {
                         nodes {
-                            id
-                            name
-                        }
-                    }
-                }
-            `,
-        }), {
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Shopify-Access-Token': process.env.SHOPIFY_TOKEN,
-            }
-        }
-    );
-
-    return response.data.data.locations.nodes[0].id;
-}
-
-async function getProductByHandle(handle) {
-    const response = await axios.post(
-        process.env.GRAPHQL_URL,
-        JSON.stringify({
-            query: `
-                query {
-                    productByHandle(handle: "${handle}") {
-                        id
-                        title
-                        variants(first: 250) {
-                            nodes {
-                                title
-                                sku
-                                inventoryQuantity
-                                inventoryItem {
-                                    id
-                                }
+                            title
+                            sku
+                            inventoryQuantity
+                            inventoryItem {
+                                id
                             }
                         }
                     }
                 }
-            `,
-        }), {
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Shopify-Access-Token': process.env.SHOPIFY_TOKEN,
             }
         }
-    );
+    `);
 
-    return response.data.data.productByHandle;
+    return data.products;
 }
 
-async function updateInventory(input) {
+async function paginateProductsByVendor(store, vendorName) {
+    const products = [];
+    let cursor = null;
+    let hasNext = true;
+
+    while (hasNext) {
+        const page = await getProductsByVendor(store, cursor, vendorName);
+        products.push(...page.nodes);
+        hasNext = page.pageInfo.hasNextPage;
+        cursor = page.pageInfo.endCursor;
+    }
+
+    return products;
+}
+
+async function updateInventory(store, input) {
     const idempotencyKey = crypto.randomUUID();
-    const response = await axios.post(
-        process.env.GRAPHQL_URL,
-        JSON.stringify({
-            query: `
-                mutation InventorySet($input: InventorySetQuantitiesInput!) {
-                    inventorySetQuantities(input: $input) @idempotent(key: "${idempotencyKey}") {
-                        inventoryAdjustmentGroup {
-                            changes {
-                                delta
-                                name
-                            }
-                        }
-                        userErrors {
-                            message
-                            field
-                        }
+    const data = await shopifyGraphQL(store, `
+        mutation InventorySet($input: InventorySetQuantitiesInput!) {
+            inventorySetQuantities(input: $input) @idempotent(key: "${idempotencyKey}") {
+                inventoryAdjustmentGroup {
+                    changes {
+                        delta
+                        name
                     }
                 }
-            `,
-            variables: {
-                input,
-            }
-        }), {
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Shopify-Access-Token': process.env.SHOPIFY_TOKEN,
+                userErrors {
+                    message
+                    field
+                }
             }
         }
-    );
+    `, { input });
 
-    return response.data.data.inventorySetQuantities.inventoryAdjustmentGroup;
+    return data.inventorySetQuantities.inventoryAdjustmentGroup;
 }
 
-module.exports = { getLocationId, getProductByHandle, updateInventory };
+module.exports = { getLocationId, paginateProductsByVendor, updateInventory };
